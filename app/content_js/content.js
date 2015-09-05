4e6168df9efe83bda4f6dabce149ddd3e593539b
Twig2let <twig2let@gmail.com>
2015-09-06 00:48:25 +0100

    Adds :not selector check for previously matched nodes. Fixes multiple <span> tag issue using a negative lookahead. Adds class to node after pattern matching contents.

--- app/content_js/content.js
+++ app/content_js/content.js
@@ -1,75 +1,76 @@
-var abbreviations = /hfig|fig|dvi|wvi|sma|db|rpi|iw|pb|dma|ath/ig;
-
-/*
- F: Above HFig 150. Plenty of space prior to Fig 200.
- A: Above DVI&WVI. 59 weeks above the weekly 20SMA.
- C: Daily DB confirmed. One (at a push, two) recent Gap Ups, above the daily 20SMA, on increased vol. Most recent (potential entry) candle is BuRPI 95.
- T: IW in play. Trend checker favourable. Bouncer plunging back below 3 after the PB. Green DMA full house. Neat PB on weekly, slightly scrappier on the Daily, but this has allowed the DB to develop, and the all important blue candles to present themselves.
- S: ATH at 185.
- */
-
 /*ToDo:
-    * Fix auto prefix recognition, int & Br,Bu etc
-    * Capture all the abbreviation data to construct regex
-    * Fix multiple string replacement issue (infinite loop)
-    * Tidy this up
-    * Write some unit tests
-    * Create tooltip
-    * Create popup options, e.g. toggle for abbreviation highlighting
-    * Test it!
-*/
+ * Fix auto prefix recognition, int & Br,Bu etc
+ * Capture all the abbreviation data to construct regex
+ * Tidy this up
+ * Create tooltip
+ * Create popup options, e.g. toggle for abbreviation highlighting
+ */
 
 
 (function () {
 
-    $('body').append("<style>.match {text-decoration:underline;color:#bfd730;font-size:12px;cursor:pointer;}</style>");
+    $('body').append("<style>.match {text-decoration:underline;color:#bfd730;cursor:pointer;}</style>");
 
-    function listener() {
-        var startTime = new Date().getTime();
-        var endTime;
+    var abbreviations = /dvi|wvi|sma|db|rpi|iw|pb|dma|ath|hfig|fig/ig;
+    var abbreviationPrefixes = /br|bu/ig;
 
-        var replacementStr = "";
-
-        $('span[data-qaid="message-text"]').each(function (index) {
-            var result;
-            var actualStr = $(this).html();
-            var strLength = actualStr.length;
-            replacementStr = actualStr;
-
-            console.info('Block' + index + ', string length: ' + strLength);
-            while (result = abbreviations.exec(actualStr)) {
-                console.info([
-                    'Block',
-                    index,
-                    ' match: ',
-                    result,
-                    ', position: ',
-                    result.index].join(''));
-
-                replacement(result);
-            }
-
-            $(this).html(replacementStr);
-
-
-        });
-        console.info('duration in seconds: ', (new Date().getTime() - startTime) / 1000);
-
-        function replacement(result) {
-            var matchStr = result[0];
-            var matchReplacement = "<span class='match' title='A tooltip'>" + matchStr + "</span>";
-            var matchPattern = '\\b' + matchStr + '\\b';
-            replacementStr = replacementStr.replace(new RegExp(matchPattern, ''), matchReplacement);
-        }
+    function addListener() {
+        document.addEventListener("DOMSubtreeModified", findPatterns, false);
     }
 
-    var timeout;
-    document.addEventListener("DOMSubtreeModified", function() {
-        if(timeout) {
-            clearTimeout(timeout);
-        }
-        timeout = setTimeout(listener, 500);
-    }, false);
+    function removeListener() {
+        document.removeEventListener("DOMSubtreeModified", findPatterns, false);
+    }
+
+    function findPatterns() {
+        // Prevent infinite loop, find patterns results in a DOMSubtreeModified event
+        removeListener();
+
+        // Loop all the relevant text nodes
+        $('span[data-qaid="message-text"]:not(.trc-matched)').each(function (index) {
+            var match;
+            var messageHtml = $(this).html();
+
+            // Test string for matches
+            while (match = abbreviations.exec(messageHtml)) {
+                console.info([
+                    'Block ',
+                    index,
+                    ' matched pattern: ',
+                    match[0],
+                    ', position: ',
+                    match.index].join(''));
+
+                // Pass any matches to the replacement function
+                messageHtml = replacement(messageHtml, match[0]);
+            }
+            $(this).html(messageHtml).addClass('trc-matched');
+        });
+
+        // Node loop exit, rebind the DOMSubtreeModified listener
+        addListener();
+    }
+
+    function replacement(messageHtml, match) {
+        var wrappedMatchPart = "<span class='match' title='A tooltip'>" + match + "</span>";
+        // Match pattern as a whole word only if it is not proceeded by a '</span>' tag
+        var matchPattern = '\\b' + match + '\\b(?!<\/span>)';
+        return messageHtml.replace(new RegExp(matchPattern, 'ig'), wrappedMatchPart);
+
+    }
+
+    function testForKnownPrefix(result) {
+    }
+
+    function insertAt(src, index, str) {
+        return src.substr(0, index) + str + src.substr(index)
+    }
+
+    //--------- FOR DEVELOPMENT ONLY ----------
+    if (!_.isEqual(location.hostname, 'www.yammer.com')) {
+        addListener();
+        $('body').append('<h3>Tee hee</h3>');
+    }
+    //-----------------------------------------
 
 }());
-
