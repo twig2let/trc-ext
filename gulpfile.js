e718ad74967c55641b3f427157ce00c4c9c6b000
Twig2let <twig2let@gmail.com>
2015-09-05 14:33:15 +0100

    Implemnts Gulp.

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var clean = require('gulp-clean');
+var flatten = require('gulp-flatten');
+var webpack = require('webpack');
+
+
+gulp.task('default', function () {
+    // place code for your default task here
+});
+
+gulp.task('build', ['clean', 'copy']);
+
+var configuration = {
+    context: __dirname + '/',
+    output: {
+        path: __dirname + "/build",
+    }
+};
+
+var paths = {
+    all: ['app/**/*.*']
+};
+
+// Delete the build directory
+gulp.task('clean', function () {
+    return gulp.src(configuration.output.path)
+        .pipe(clean());
+});
+
+// Copy all other files to dist directly
+gulp.task('copy', function () {
+    return gulp.src(paths.all, {cwd: configuration.context})
+        .pipe(flatten())
+        .pipe(gulp.dest(configuration.output.path));
+});
