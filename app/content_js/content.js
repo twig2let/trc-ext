758061ff53f248fc79c94862d9907240bebfd6d6
Twig2let <twig2let@gmail.com>
2015-09-06 19:35:29 +0100

    Updates abbrevs

--- app/content_js/content.js
+++ app/content_js/content.js
@@ -16,17 +16,79 @@
             bu: 'Bullish',
             br: 'Bearish'
         },
-        rpi: 'Relative Price Indicator',
-        dvi: 'Daily Value Index',
-        wvi: 'Weekly Value Index',
-        sma: 'Simple Moving Average',
-        db: 'Double Bottom',
-        iw: 'Impulse Wave',
-        pb: 'Pullback',
-        dma: '???',
-        ath: 'All Time High',
-        hfig: 'Half Figure',
-        fig: 'The Figure'
+        '1R': '– Target is 100% of Risk (distance from entry to stop)',
+        ABS: 'Advanced Breakout System',
+        AE: 'Alternative Entry',
+        BaC: 'Break and Close',
+        BD: 'Break Down',
+        BE: 'Break Even',
+        BO: 'B/o – Break Out',
+        BoB: 'Break Out Bar',
+        BoBCAT: 'Break Out Bar Category (e.g 4 BoBCats)',
+        BrE: 'Bearish Engulfing',
+        BrF: 'Bearish Flag',
+        BrRPI: 'Bearish RPI',
+        BuE: 'Bullish Engulfing',
+        BuF: 'Bullish Flag',
+        BuRPI: 'Bullish RPI',
+        BW: 'Bollinger Bandwidth',
+        BZ: 'Buy Zone',
+        C4H: 'C4L – Cat 4 High or Cat 4 Low',
+        CAT: 'Category (e.g Cat 4)',
+        CoE: 'Confluence of Events',
+        CT: 'Channel Tunnel',
+        CTS: 'Collapsible Trailing Stop',
+        DB: 'Double Bottom',
+        DD: 'Double Deviation',
+        DnT: 'Do Not Trade',
+        DMA: 'Dual Moving Average',
+        DT: 'Double Top',
+        DVI: 'Daily Value Index',
+        EA: 'Entry Advantage',
+        EMMA: 'Exponential Multiple <Mving Average',
+        ESLT: 'Entry Stop Loss Technique',
+        EW: 'Elliott Waves',
+        FACTS: 'Figure, VI, Chart / Candlestick pattern, Trend, Support / Resistance levels',
+        FT: 'Free Trade',
+        GO: 'Gator Oscillator',
+        'HH/LL': 'Highest High / Lowest Low (Donchian levels)',
+        IB: 'Initial Breakout',
+        IW: 'Impulse Wave',
+        LHF: 'Low Hanging Fruit',
+        LT: 'Long Term',
+        MT: 'Medium Term',
+        OO: 'Obvious Opportunity: ',
+        'P&P': 'Price and Personaility',
+        PB: 'P/b – Pullback',
+        PL: 'P/L – Pink Line',
+        PoA: 'Plan of Action',
+        PP: 'Personal Preference',
+        PT: 'Pilot Trade',
+        PuB: 'Pullback Bar',
+        RBO: 'Range Break Out',
+        RBOA: 'Range Breakout Advanced',
+        RC: 'Reversal Candle',
+        RN: 'Round Number',
+        RF: 'Risk Free',
+        RPI: 'Range Percentage Indicator',
+        RR: 'R Ratio',
+        RST: 'Resistance, Support and Trendline',
+        RTC: 'Regression Trend Channel',
+        RZ: 'Range Zone',
+        SD: 'Standard Deviation',
+        ST: 'Short Term',
+        STEP: 'Stop to Entry Point',
+        SZ: 'Sell Zone',
+        T1: 'First Target',
+        TAB: 'Turnaround Bar',
+        TB: 'Triple Bottom',
+        TC: 'Trend Check',
+        TL: 'Trendline',
+        'TL BoB': 'Trendline Break or Bounce',
+        TSH: 'TSL – Trailing Stop High / Trailing Stop Low',
+        TT: 'Triple Top',
+        VI: 'Value Index',
+        WVI: 'Weekly Value Index'
     };
 
     function addListener() {
@@ -77,7 +139,7 @@
     }
 
     function getTooltipText(match) {
-        if (_.isUndefined(match[1])){
+        if (_.isUndefined(match[1])) {
             return abbrsLookup[match[0].toLowerCase()];
         } else {
             return [
@@ -88,9 +150,10 @@
         }
     }
 
+    addListener();
+
     //--------- FOR DEVELOPMENT ONLY ----------
     if (!_.isEqual(location.hostname, 'www.yammer.com')) {
-        addListener();
         $('body').append('<h3>Tee hee</h3>');
     }
     //-----------------------------------------
