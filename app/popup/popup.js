30f65e8d4afed33d7b6601d67c325fc0c593dcb6
Twig2let <twig2let@gmail.com>
2015-09-29 23:56:46 +0100

    tests: Creates an additional assertion in the popup.spec suite.

--- app/popup/popup.js
+++ app/popup/popup.js
@@ -10,8 +10,14 @@
             chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
                 chrome.tabs.sendMessage(tabs[0].id, {messageId: messageId, value: !config[configKey]}, function () {});
             });
+            updateConfigSettings(configKey, config);
         });
     });
+
+    function updateConfigSettings(configKey, config) {
+        config[configKey] = !config[configKey]; // Flip the current value (toggle)
+        chrome.storage.sync.set({'config': config}, function () {});
+    }
 }());
 
 
--- test/popup.spec.js
+++ test/popup.spec.js
@@ -6,7 +6,7 @@ describe('Popup JS', function () {
 
     describe('Toggle Abbreviations Button', function () {
         describe('On Click', function () {
-            it('Should fetch the state from the local config, flip the state value and pass a message to the content js', function () {
+            it('Should fetch the state from the local config, flip the state value and pass a message to the content js and store the new config', function () {
 
                 var config = {
                     abbrHighlighting: false
@@ -26,11 +26,17 @@ describe('Popup JS', function () {
                     value: true
                 };
                 var expectedCallCount = 1;
+                var newConfig = chrome.storage.sync.set.args[0][0].config;
+                var expectedConfig = {
+                    config: {
+                        abbrHighlighting: true
+                    }
+                };
 
                 chai.assert.deepEqual(passedObject, expectedObject, 'Object passed in message is incorrect');
                 chai.assert.strictEqual(chrome.tabs.sendMessage.callCount, expectedCallCount, 'chrome.tabs.sendMessage should have been called once');
-
-            })
+                chai.assert.deepEqual(newConfig, expectedConfig.config, 'New config is incorrect');
+            });
         })
     })
 });
