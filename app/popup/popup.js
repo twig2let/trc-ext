f60452acdb17a584984a93c123775e9ede581b59
Twig2let <twig2let@gmail.com>
2015-09-30 12:10:28 +0100

    tests: More popup.js unit tests

--- app/popup/popup.js
+++ app/popup/popup.js
@@ -1,23 +1,29 @@
 (function () {
 
-    var toggleAbbreviationsBtn = document.getElementById('toggleAbbreviationsBtn');
+    var $toggleAbbreviationsBtn = $('#toggleAbbreviationsBtn');
 
-    toggleAbbreviationsBtn.addEventListener('click', function () {
+    $toggleAbbreviationsBtn.on('click', function () {
         var messageId = 'trc_abbreviations';
         var configKey = 'abbrHighlighting';
 
         chrome.storage.sync.get('config', function (config) {
+            config[configKey] = !config[configKey];
             chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, {messageId: messageId, value: !config[configKey]}, function () {});
+                chrome.tabs.sendMessage(tabs[0].id, {messageId: messageId, value: config[configKey]}, function () {});
             });
-            updateConfigSettings(configKey, config);
+            updateConfigSettings(config);
+            toggleBtnState(config[configKey]);
         });
     });
 
-    function updateConfigSettings(configKey, config) {
-        config[configKey] = !config[configKey]; // Flip the current value (toggle)
+    function toggleBtnState(state) {
+        $toggleAbbreviationsBtn.toggleClass('enabled', state);
+    }
+
+    function updateConfigSettings(config) {
         chrome.storage.sync.set({'config': config}, function () {});
     }
+
 }());
 
 
--- test/popup.spec.js
+++ test/popup.spec.js
@@ -2,40 +2,153 @@ describe('Popup JS File', function () {
 
     afterEach(function () {
         chrome.tabs.sendMessage.reset();
+        chrome.storage.sync.set.reset();
+        chrome.storage.sync.get.reset();
     });
 
     describe('Toggle Abbreviations Button', function () {
         describe('On Click', function () {
-            it('Should fetch the state from the local config, flip the state value and pass a message to the content js and store the new config', function () {
+            describe('when the existing abbrHighlighting key is false', function () {
+                it('should pass message to content abbreviation_highlighting js with appropriate message id and new toggle state', function () {
 
-                var config = {
-                    abbrHighlighting: false
-                };
-                var mockTabs = [{
-                    id: 'tab1'
-                }];
+                    var config = {
+                        abbrHighlighting: false
+                    };
+                    var mockTabs = [{
+                        id: 'tab1'
+                    }];
+                    var $toggleAbbreviationsBtn = $('#toggleAbbreviationsBtn');
 
-                chrome.storage.sync.get.yields(config);
-                chrome.tabs.query.yields(mockTabs);
+                    chrome.storage.sync.get.yields(config);
+                    chrome.tabs.query.yields(mockTabs);
 
-                $('#toggleAbbreviationsBtn').click();
+                    $toggleAbbreviationsBtn.click();
 
-                var passedObject = chrome.tabs.sendMessage.args[0][1];
-                var expectedObject = {
-                    messageId: "trc_abbreviations",
-                    value: true
-                };
-                var expectedCallCount = 1;
-                var newConfig = chrome.storage.sync.set.args[0][0].config;
-                var expectedConfig = {
-                    config: {
+                    var passedObject = chrome.tabs.sendMessage.args[0][1];
+                    var expectedObject = {
+                        messageId: "trc_abbreviations",
+                        value: true
+                    };
+                    var expectedCallCount = 1;
+
+                    chai.assert.deepEqual(passedObject, expectedObject, 'Object passed in message is incorrect');
+                    chai.assert.strictEqual(chrome.tabs.sendMessage.callCount, expectedCallCount, 'chrome.tabs.sendMessage should have been called once');
+                });
+
+                it('should update the settings config with the new settings', function () {
+
+                    var config = {
+                        abbrHighlighting: false
+                    };
+                    var mockTabs = [{
+                        id: 'tab1'
+                    }];
+                    var $toggleAbbreviationsBtn = $('#toggleAbbreviationsBtn');
+
+                    chrome.storage.sync.get.yields(config);
+                    chrome.tabs.query.yields(mockTabs);
+
+                    $toggleAbbreviationsBtn.click();
+
+                    var newConfig = chrome.storage.sync.set.args[0][0].config;
+                    var expectedConfig = {
+                        config: {
+                            abbrHighlighting: true
+                        }
+                    };
+
+                    chai.assert.deepEqual(newConfig, expectedConfig.config, 'New config is incorrect');
+                    //chai.assert.isTrue($toggleAbbreviationsBtn.hasClass('enabled'), 'Button should have the class of enabled');
+                });
+
+                it('should add the \'enabled\' class to the toggleAbbreviationsBtn', function () {
+
+                    var config = {
+                        abbrHighlighting: false
+                    };
+                    var mockTabs = [{
+                        id: 'tab1'
+                    }];
+                    var $toggleAbbreviationsBtn = $('#toggleAbbreviationsBtn');
+
+                    chrome.storage.sync.get.yields(config);
+                    chrome.tabs.query.yields(mockTabs);
+
+                    $toggleAbbreviationsBtn.click();
+
+                    chai.assert.isTrue($toggleAbbreviationsBtn.hasClass('enabled'), 'Button should have the class of enabled');
+                });
+            });
+
+            describe('when the existing abbrHighlighting key is true', function () {
+                it('should pass message to content abbreviation_highlighting js with appropriate message id and new toggle state', function () {
+
+                    var config = {
                         abbrHighlighting: true
-                    }
-                };
+                    };
+                    var mockTabs = [{
+                        id: 'tab1'
+                    }];
+                    var $toggleAbbreviationsBtn = $('#toggleAbbreviationsBtn');
 
-                chai.assert.deepEqual(passedObject, expectedObject, 'Object passed in message is incorrect');
-                chai.assert.strictEqual(chrome.tabs.sendMessage.callCount, expectedCallCount, 'chrome.tabs.sendMessage should have been called once');
-                chai.assert.deepEqual(newConfig, expectedConfig.config, 'New config is incorrect');
+                    chrome.storage.sync.get.yields(config);
+                    chrome.tabs.query.yields(mockTabs);
+
+                    $toggleAbbreviationsBtn.click();
+
+                    var passedObject = chrome.tabs.sendMessage.args[0][1];
+                    var expectedObject = {
+                        messageId: "trc_abbreviations",
+                        value: false
+                    };
+                    var expectedCallCount = 1;
+
+                    chai.assert.deepEqual(passedObject, expectedObject, 'Object passed in message is incorrect');
+                    chai.assert.strictEqual(chrome.tabs.sendMessage.callCount, expectedCallCount, 'chrome.tabs.sendMessage should have been called once');
+                });
+
+                it('should update the settings config with the new settings', function () {
+
+                    var config = {
+                        abbrHighlighting: true
+                    };
+                    var mockTabs = [{
+                        id: 'tab1'
+                    }];
+                    var $toggleAbbreviationsBtn = $('#toggleAbbreviationsBtn');
+
+                    chrome.storage.sync.get.yields(config);
+                    chrome.tabs.query.yields(mockTabs);
+
+                    $toggleAbbreviationsBtn.click();
+
+                    var newConfig = chrome.storage.sync.set.args[0][0].config;
+                    var expectedConfig = {
+                        config: {
+                            abbrHighlighting: false
+                        }
+                    };
+
+                    chai.assert.deepEqual(newConfig, expectedConfig.config, 'New config is incorrect');
+                });
+
+                it('should add the \'enabled\' class to the toggleAbbreviationsBtn', function () {
+
+                    var config = {
+                        abbrHighlighting: true
+                    };
+                    var mockTabs = [{
+                        id: 'tab1'
+                    }];
+                    var $toggleAbbreviationsBtn = $('#toggleAbbreviationsBtn');
+
+                    chrome.storage.sync.get.yields(config);
+                    chrome.tabs.query.yields(mockTabs);
+
+                    $toggleAbbreviationsBtn.click();
+
+                    chai.assert.isFalse($toggleAbbreviationsBtn.hasClass('enabled'), 'Button should have the class of enabled');
+                });
             });
         })
     })
