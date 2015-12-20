8e757add76216136da042e99e3b636109da2186c
Twig2let <twig2let@gmail.com>
2015-12-20 20:54:25 +0000

    Adds a gulp release task, uglifys.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var seq = require('gulp-sequence');
 var flatten = require('gulp-flatten');
 var del = require('del');
-
+var uglify = require('gulp-uglify');
 
 
 gulp.task('default', function () {
@@ -10,10 +10,11 @@ gulp.task('default', function () {
 
 gulp.task('build', seq('clean', 'copy'));
 
+gulp.task('release', seq('clean', 'copy', 'uglify'));
 var configuration = {
     context: __dirname + '/',
     output: {
-        path: __dirname + "/build"
+        build: __dirname + "/build"
     }
 };
 
@@ -24,12 +25,18 @@ var paths = {
 
 // Delete the build directory
 gulp.task('clean', function () {
-    return del(configuration.output.path);
+    return del([configuration.output.build, 'build.zip']);
 });
 
 // Copy all other files to dist directly
 gulp.task('copy', function () {
     return gulp.src(paths.all, {cwd: configuration.context})
         .pipe(flatten())
-        .pipe(gulp.dest(configuration.output.path));
+        .pipe(gulp.dest(configuration.output.build));
+});
+
+gulp.task('uglify', function () {
+    return gulp.src(configuration.output.build + '/**/*.js', {cwd: configuration.context})
+        .pipe(uglify())
+        .pipe(gulp.dest(configuration.output.build));
 });
